0360a29d40b2e37910755897adfdc2448a6e629e
amuniz2 <anam@anambp15.hsd1.fl.comcast.net>
2014-10-19 01:03:11 -0400

    Fixed problem that caused new list to not be created correctly.

---
+++ HoneyGoGroceryShopping/Controllers/HGGSGroceryItemPictureViewController.h
@@ -0,0 +1,13 @@
+//
+//  HGGSGroceryItemPictureViewController.h
+//  HoneyGoGroceryShopping
+//
+//  Created by Ana Muniz on 9/17/14.
+//  Copyright (c) 2014 Ana Muniz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGGSGroceryItemPictureViewController : UIViewController
+
+@end
---
+++ HoneyGoGroceryShopping/Controllers/HGGSGroceryItemPictureViewController.m
@@ -0,0 +1,49 @@
+//
+//  HGGSGroceryItemPictureViewController.m
+//  HoneyGoGroceryShopping
+//
+//  Created by Ana Muniz on 9/17/14.
+//  Copyright (c) 2014 Ana Muniz. All rights reserved.
+//
+
+#import "HGGSGroceryItemPictureViewController.h"
+
+@interface HGGSGroceryItemPictureViewController ()
+
+@end
+
+@implementation HGGSGroceryItemPictureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
